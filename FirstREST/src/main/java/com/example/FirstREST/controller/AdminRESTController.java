eaed8e565a441a673b3fba156d25e57801d46926
Seawitcher <Seawitcher83@yandex.ru>
2022-12-20 00:07:35 +0400

    Version 0.4 (add editUser)

--- FirstREST/src/main/java/com/example/FirstREST/controller/AdminRESTController.java
+++ FirstREST/src/main/java/com/example/FirstREST/controller/AdminRESTController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -72,11 +73,12 @@ public class AdminRESTController {
     }
 
 
-    @PutMapping("/{id}/editUser")
-    public String userSaveEdit(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
+    @PatchMapping("/editUser/{id}")
+    public ResponseEntity<HttpStatus> userSaveEdit(@RequestBody User user, @PathVariable("id") Long id) {
+        user.setId(id);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userService.editUser(user);
-        return "redirect:/admin";
+        return new ResponseEntity<> (HttpStatus.OK);
     }
 
 }
